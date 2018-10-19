65e48fc5b3809ac1932c1b03f22832aaa1300be2
Jessica Skok <jessica.skok@gmail.com>
2018-10-19 16:51:28 -0700

    set up basic server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/youtubesubs.php'));
+});
+
+
+app.listen(PORT, () => console.log('Running on port 3000! 🦄'));
